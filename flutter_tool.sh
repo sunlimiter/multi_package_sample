02e8dee92c404a82b027fae7754c8c031a21b12c
lty <litianyi@psdculture.com>
2026-01-29 09:29:00 +0800

    update .gitignore

--- flutter_tool.sh
+++ flutter_tool.sh
@@ -25,12 +25,12 @@ done
 
 echo $1 'and' $2
 echo $DART_DEFINES
-#cd app
-#if [ $1 == 'run' ]
-#then
-#  fvm flutter $1 -t $TARGET $DART_DEFINES
-#fi
-#if [ $1 == 'build' ]
-#then
-#  fvm flutter $1 $2 -t $TARGET $DART_DEFINES
-#fi
+cd app
+if [ $1 == 'run' ]
+then
+ fvm flutter $1 -t $TARGET $DART_DEFINES
+fi
+if [ $1 == 'build' ]
+then
+ fvm flutter $1 $2 -t $TARGET $DART_DEFINES
+fi
